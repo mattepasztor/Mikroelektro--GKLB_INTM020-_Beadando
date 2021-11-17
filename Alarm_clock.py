9f4bccd2311e40ba6dcfea680c850b34b6cfd0a6
Mate Pasztor <pasztor.mate91@gmail.com>
2021-11-17 22:21:04 +0100

    Loop létrehozás
    Loop létrehozás

--- Alarm_clock.py
+++ Alarm_clock.py
@@ -1,5 +1,14 @@
- #Zenelejátszás Python segítségével
+ #Zenelejátszás Python segítségével & Loop létrehozás
  
  import vlc
  player = vlc.MediaPlayer("/home/pi/Music/music.mp3")
  player.play()
+ 
+ vlc_instance = vlc.Instance("--input-repeat=999")
+ music = vlc_instance.media_new("/home/pi/Music/music.mp3")
+ 
+ player.set_media(music)
+ player.audio_set_volume(100)
+ player.play()
+ 
+ 
